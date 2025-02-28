bbe74d383a704439634ec1b717f51c1609f14499
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-02-28 13:04:32 +0200

    first commit

---
+++ shopping-app/src/Data.jsx
@@ -0,0 +1,43 @@
+const products = [
+    {
+        id:1,
+        name:"iphone14",
+        category:"Electronics",
+        price:999
+    },
+
+    {
+        id:2,
+        name:"samsungGalaxy S23",
+        category:"Electronics",
+        price:899
+    },
+
+    {
+        id:3,
+        name:"Nike Air Max",
+        category:"Fashion",
+        price:120
+    },
+
+    {
+    id:4,
+    name:"Adidas Sneakers",
+    category:"Fashion",
+    price:150
+    },
+
+    {
+        id:5,
+        name:"MacBook Pro",
+        category:"Electronics",
+        price:1999
+    },
+
+    {
+        id:6,
+        name:"Leather Jacket",
+        category:"Fashion",
+        price:300
+    },
+]
