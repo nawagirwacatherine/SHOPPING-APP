0ebec14efeb995dbf23bc90a360ace8ed54bef0c
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-03-06 21:44:01 +0200

    Export a component to  spur

--- shopping-app/src/Pages/Spur.jsx
+++ shopping-app/src/Pages/Spur.jsx
@@ -1,5 +1,6 @@
 import BodyScrub from "../components/spur-products/BodyScrub.jsx"
 import Massage from "../components/spur-products/Massage.jsx"
+import MicroBladding from "../components/spur-products/MicroBladding.jsx"
 
 const Spur = () => {
     return(
@@ -7,6 +8,7 @@ const Spur = () => {
   <h1>Spur</h1>
   < BodyScrub/>
   < Massage/>
+ < MicroBladding/>
   <div>
 
 
---
+++ shopping-app/src/components/spur-products/MicroBladding.jsx
@@ -0,0 +1,21 @@
+import "./microbladding.css"
+import microneddling from "../../images/microneddling.webp"
+
+
+const MicroBladding = () =>{
+    return (
+     <div className="body-scrub">
+      <h2>Body Massage</h2>
+
+      <div className="bodyscrub-products">
+        <img src={microneddling} alt="" className="bodyscrub-img"/>
+      </div>
+
+      <div>
+        <h3>Relax every body cell</h3>
+      </div>
+     </div>
+    )
+}
+
+export default MicroBladding;
